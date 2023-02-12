81d698bb0f736cebfccea80cbcb29e4913aa82ba
Pavel-Gs <pavelgen@gmail.com>
2023-02-12 08:35:36 -0800

    changed some comments

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -6,16 +6,20 @@ import {useState, useEffect} from 'react';
 // DISCLAIMER STRUCTURE FUNCTION
 export const DisclaimerStructure = () => {
 	const [visibility, setVisibility] = useState({ opacity: '0' });		// set the initial opacity to 0
-	useEffect(() => {													// fade-in the disclaimer on component-load after the delay
+	useEffect(() => {													// fade-IN the disclaimer on component-load after the delay
 		setVisibility({ opacity: '1', transition: '0.5s ease-in 2s' });
 	}, []);
 
-	const transparentDisclaimer = () => {								// handle the disclaimer's transparency
+
+	const transparentDisclaimer = () => {								// handle the disclaimer's transparency on click (fade-OUT)
 		setVisibility({ opacity: '0', transition: '0.5s ease-out' });
 	};
-	const hideDisclaimer = () => {										// handle the disclaimer's display state
+
+
+	const hideDisclaimer = () => {										// handle the disclaimer's display state on the same click
 		setVisibility({ display: 'none' });
 	};
+
 
 	return (<section id='disclaimer-section' style={visibility}>		{/* visibility styles applies here */}
 		<p>This is my web-development portfolio site.<br />
