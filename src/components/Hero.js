9c813b934ce0bd1c81c3daa5f5ad6e6ef16251c1
Pavel-Gs <pavelgen@gmail.com>
2025-04-25 13:50:06 -0700

    Updated hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@
 export const HeroStructure = () => {
 	return (<section className='chapter' id='hero-section'>
 		<article>
-			<h1>PRIVATE GEOMATICS CONTRACTOR</h1>
+			<h1>GEOMATICS CONTRACTOR</h1>
 			<p>specializing in land surveying, civil projects and cadastre*</p>
 			<a href='#about-section'>LEARN MORE</a>
 		</article>
